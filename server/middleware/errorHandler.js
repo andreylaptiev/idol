1015a2af33a3bb718c8a6883e5654fe2e7c984bc
Andrey Laptiev <andreylaptiev13@gmail.com>
2022-04-04 18:24:58 +0300

    Add error handling middleware

---
+++ server/middleware/errorHandler.js
@@ -0,0 +1,10 @@
+const PageNotFoundError = require('../errors/PageNotFoundError');
+
+module.exports = function(err, req, res, next) {
+  if (err instanceof PageNotFoundError) {
+    console.error(err.status, err.message);
+    console.error(err.stack);
+    return res.json({ code: err.status, message: err.message });
+  }
+  return res.send('Unexpected Error!');
+}
