fac183afe67376bd89edda90a56d49e263efc334
Andrey Laptiev <andreylaptiev13@gmail.com>
2022-04-05 20:16:50 +0300

    Add 'express-fileupload' package
    Add file related middleware to server App

--- server/index.js
+++ server/index.js
@@ -2,6 +2,8 @@ require('dotenv').config();
 const express = require('express');
 const mongodb = require('./db');
 const cors = require('cors');
+const fileUpload = require('express-fileupload');
+const path = require('path');
 const usersRouter = require('./routes/users');
 const adminRouter = require('./routes/admin/admin');
 const errorHandler = require('./middleware/errorHandler');
@@ -12,6 +14,8 @@ const PORT = process.env.PORT || 5000;
 const app = express();
 
 app.use(cors());
+app.use(express.static(path.resolve(__dirname, 'static')));
+app.use(fileUpload({}));
 
 // routing
 app.use('/admin', adminRouter);
@@ -29,8 +33,6 @@ const start = async () => {
     });
   } catch (e) {
     console.error(e);
-  } finally {
-    await mongodb.close();
   }
 }
 
