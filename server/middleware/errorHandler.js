c0da58e2403b4f6bec925adb018c2d6a021b7f10
Andrey Laptiev <andreylaptiev13@gmail.com>
2022-04-07 19:22:15 +0300

    Add default error handling process
    Just console log error if there is no specific class for handling
    type of thrown error.

--- server/middleware/errorHandler.js
+++ server/middleware/errorHandler.js
@@ -6,5 +6,6 @@ module.exports = function(err, req, res, next) {
     console.error(err.stack);
     return res.json({ code: err.status, message: err.message });
   }
+  console.error(err.stack);
   return res.send('Unexpected Error!');
 }
