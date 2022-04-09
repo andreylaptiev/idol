d2372143f7f811942b814407670fce59996d8689
Andrey Laptiev <andreylaptiev13@gmail.com>
2022-04-09 18:30:28 +0300

    Refactor namings and remove '/auth' route

--- server/routes/users.js
+++ server/routes/users.js
@@ -1,15 +1,13 @@
 const express = require('express');
 const router = express.Router();
-const UsersController = require('../controllers/UsersController');
+const userController = require('../controllers/userController');
 const PageNotFoundError = require('../errors/PageNotFoundError');
 
 // GET and POST methods are handled in a single method
-router.use('/auth', UsersController.checkAuth);
-router.use('/login', UsersController.login);
-router.use('/signup', UsersController.signup);
+router.use('/login', userController.login);
+router.use('/signup', userController.signup);
 
-// throw error 404 if we are going to any route except
-// 'users/login' or 'users/signup'
+// throw error 404 if route is not 'users/login' or 'users/signup'
 router.all(/^(?!login|signup).*/, (req, res, next) => {
   next(new PageNotFoundError());
 });
