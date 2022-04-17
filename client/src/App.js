9e4de16ec57e2b2269c61110a024ad9272eadf36
Andrey Laptiev <andreylaptiev13@gmail.com>
2022-04-17 13:03:42 +0300

    Add App styles and components: Header and Outlet

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,15 @@
 import React from 'react';
+import { Outlet } from 'react-router-dom';
+import './App.css';
+import Header from './components/Header/Header';
 
 const App = () => {
   return (
-    <div>
-      
+    <div className='app'>
+      <Header />
+      <div className='main'>
+        <Outlet />
+      </div>
     </div>
   );
 }
