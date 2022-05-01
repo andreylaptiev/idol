5d5adc1c63d14bcf01428df70c1f3e84f1f9d8a6
Andrey Laptiev <andreylaptiev13@gmail.com>
2022-05-01 18:49:15 +0300

    Add UserProvider component

--- client/src/App.js
+++ client/src/App.js
@@ -2,13 +2,16 @@ import React from 'react';
 import { BrowserRouter } from 'react-router-dom';
 import AppRouter from './routes/AppRouter';
 import { AuthProvider } from './contexts/AuthContext';
+import { UserProvider } from './contexts/UserContext';
 
 const App = () => {
   return (
     <AuthProvider>
-      <BrowserRouter>
-        <AppRouter />
-      </BrowserRouter>
+      <UserProvider>
+        <BrowserRouter>
+          <AppRouter />
+        </BrowserRouter>
+      </UserProvider>
     </AuthProvider>
   );
 }
