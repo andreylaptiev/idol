ad6a81bc24a8f41d57c1875326427721aef08c96
Andrey Laptiev <andreylaptiev13@gmail.com>
2022-05-01 19:37:27 +0300

    Add Layout component, AdminRoute and Unauth routes

--- client/src/routes/AppRouter.jsx
+++ client/src/routes/AppRouter.jsx
@@ -4,28 +4,36 @@ import Home from './Home';
 // public routes
 import Auth from './Auth';
 import Gallery from './Gallery';
+import Layout from './Layout';
 import News from './News';
 import Shop from './Shop';
+import Unauthorized from './Unauthorized';
+// checks user role before entering admin panel
+import AdminRoute from './AdminRoute';
 // admin panel routes
 import Admin from './Admin';
-import AdminArticles from './Admin/AdminArticles'
-import AdminGallery from './Admin/AdminGallery'
-import AdminProducts from './Admin/AdminProducts'
+import AdminArticles from './Admin/AdminArticles';
+import AdminGallery from './Admin/AdminGallery';
+import AdminProducts from './Admin/AdminProducts';
 
 function AppRouter() {
   return (
     <Routes>
-      <Route path="/" element={<Home />}>
+      <Route path="/" element={<Layout />}>
+        <Route path="/" element={<Home />} />
         <Route path="news" element={<News />} />
         <Route path="gallery" element={<Gallery />} />
         <Route path="shop" element={<Shop />} />
         <Route path="login" element={<Auth />} />
         <Route path="signup" element={<Auth />} />
-        <Route path="admin" element={<Admin />}>
-          <Route path="articles" element={<AdminArticles />} />
-          <Route path="gallery" element={<AdminGallery />} />
-          <Route path="products" element={<AdminProducts />} />
+        <Route element={<AdminRoute />}>
+          <Route path="admin" element={<Admin />}>
+            <Route path="articles" element={<AdminArticles />} />
+            <Route path="gallery" element={<AdminGallery />} />
+            <Route path="products" element={<AdminProducts />} />
+          </Route>
         </Route>
+        <Route path="unauthorized" element={<Unauthorized />} />
       </Route>
     </Routes>
   );
