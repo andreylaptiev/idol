db65ebe390cd4b90dbcaa1fa3beb10efd55ade2a
Andrey Laptiev <andreylaptiev13@gmail.com>
2022-05-01 19:29:42 +0300

    Extract app layout from Home component
    Now Home component is a container component for '/' route. App basic
    layout is in Layout component

--- client/src/routes/Home/index.jsx
+++ client/src/routes/Home/index.jsx
@@ -1,18 +1,10 @@
 import React from 'react';
-import { Outlet } from 'react-router-dom';
-import { NavbarProvider } from '../../contexts/NavbarContext';
-import Header from '../../components/Header/Header';
 import styles from './Home.module.css';
 
 const Home = () => {
   return (
-    <div className={styles.app}>
-      <NavbarProvider>
-        <Header />
-      </NavbarProvider>
-      <div className={styles.main}>
-        <Outlet />
-      </div>
+    <div className={styles.home}>
+      Home
     </div>
   );
 }
