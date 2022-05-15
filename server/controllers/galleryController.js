f4bbf99203850c40f0be7b8b7968865c056c0ddb
Andrey Laptiev <andreylaptiev13@gmail.com>
2022-05-15 19:51:08 +0300

    Refactor uploadImage method
    Add functions to process uploaded images: set unique name,
    move to storage and insert to database
    
    Also refactor uploadImage method to use above functions

--- server/controllers/galleryController.js
+++ server/controllers/galleryController.js
@@ -3,23 +3,67 @@ const { rm } = require('fs/promises');
 const path = require('path');
 const generateId = require('../utils/generateId');
 
+// img must be a list of objects representing uploaded images
+// set unique name for each image by modifying existing array
+const setUniqueImageNames = (img) => {
+  for (let i of img) {
+    i.name = `${Date.now()}-${i.name}`;
+    console.log(i.name);
+  }
+}
+
+const moveImagesToStorage = (img) => {
+  let storage;
+  try {
+    for (let i of img) {
+      storage = path.resolve(__dirname, '../static/gallery', i.name);
+      i.mv(storage);
+    }
+  } catch(err) {
+    console.log(err);
+    return err;
+  }
+}
+
+const insertImagesToDb = async (img) => {
+  let id;
+  let path;
+  let query;
+  const collection = client.db('idol').collection('gallery');
+  try {
+    for (let i of img) {
+      id = await generateId();
+      // image path related to 'static' folder
+      // which our express app uses as source of static files
+      path = `/gallery/${i.name}`;
+      query = { _id: id, image: path };
+      await collection.insertOne(query);
+    }
+  } catch(err) {
+    console.log(err);
+    return err;
+  }
+}
+
 class GalleryController {
   async uploadImage(req, res, next) {
+    // processing: set unique name -> move to storage -> insert to db
+    // req.files looks like this:
+    // 1 image: { images: {...} }
+    // 2+ images: { images: [ {...}, {...} ] }
     try {
-      // specify unique name for uploaded image and move it to storage
-      const { image } = req.files;
-      const imageName = `${Date.now()}-${image.name}`;
-      const storage = path.resolve(__dirname, '../static/gallery', imageName);
-      image.mv(storage);
-
-      // add image to database
-      const collection = client.db('idol').collection('gallery');
-      const id = await generateId();
-      const query = { _id: id, image: imageName };
-      const insertImage = await collection.insertOne(query);
-
-      // respond if success
-      res.send(`Uploaded successfully: ${imageName}`);
+      let { images } = req.files;
+      // by default we expect multiple images (an array of objects)
+      // in case of a single uploaded image we add it to array
+      if (!Array.isArray(images)) {
+        images = [images];
+      }
+      // process array of uploaded image(s)
+      setUniqueImageNames(images);
+      moveImagesToStorage(images);
+      insertImagesToDb(images);
+      // response in case of success
+      res.json({ message: 'Image(s) uploaded successfully' });
     } catch(err) {
       // in case of any error send error status 500
       res.sendStatus(500);
